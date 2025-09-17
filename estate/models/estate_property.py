0fc9511857708e1ce4bc93a1f75e4cfe6e9627a6
Emmanuelle Alfeef <e.alfeef@omnilog.com>
2025-09-17 09:32:29 +0200

    add search

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,7 +8,7 @@ class EstateProperty(models.Model):
     description = fields.Text(string="Description")
     date_availability = fields.Date()
     expected_price = fields.Float(string="Expected Price", required=True)
-    selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
+    selling_price = fields.Float(string="Selling Price", copy=False, required=True)
     bedrooms = fields.Integer(string="Bedrooms", default=2)
     garden = fields.Boolean(string="Garden")
     garden_orientation = fields.Selection(
